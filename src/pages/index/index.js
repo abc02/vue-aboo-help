607d5027df85db788b85b436b5aa8bae810fe02b
abc02 <abcgg02@gmail.com>
2017-12-08 15:39:24 +0800

    location newwork

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import { Cell } from 'mint-ui'
 Vue.component(Cell.name, Cell)
+
+import logo from './assets/logo.png'
+import phone from './assets/phone.png'
 new Vue({
   el: '#app',
   data: {
@@ -12,15 +15,25 @@ new Vue({
       {
         text: '功能介绍',
         url: 'app_use.html'
+      },
+      {
+        text: '定位失败原因',
+        url: 'location.html'
+      },
+      {
+        text: '设备无法联网',
+        url: 'network.html'
       }
     ],
     contact: [
       {
+        icon: phone,
         text: '全国服务热线 (周一至周五9:00-17:00)',
         label: '0577-88789089',
         url: 'tel:0577-88789089'
       },
       {
+        icon: logo,
         text: '阿布跑跑官方网站',
         label: 'http://0577xiedu.net',
         url: 'http://0577xiedu.net'
---
+++ src/pages/network/network.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import '@/styles/reset.scss'
+import '@/styles/base.scss'
+
+new Vue({
+  el: '#app',
+  data: {
+    title: '设备无法联网处理方法',
+    content: [
+      {
+        title: '无法联网',
+        text: [
+            '可以尝试换个环境或者等一会儿重试。'
+        ]
+      },
+      {
+        title: '其他情况',
+        text: [
+            '1.设备可能已关机，此时需要将设备开机，如果不能开机建议对设备进行充电。',
+            '2.设备中的SIM卡可能已经欠费停机，此时需要联系客服进行查询。'
+        ]
+      }
+    ]
+  }
+})
